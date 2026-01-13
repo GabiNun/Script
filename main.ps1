b5304a64f0e4acf23b3096b0976338a5aedd6d61
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-13 16:17:14 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -54,6 +54,6 @@ sc.exe delete edgeupdate
 sc.exe delete edgeupdatem
 Unregister-ScheduledTask *Edge* -Confirm:$False
 
-$Value = "Start-Process powershell -ArgumentList '-Command `"irm pastebin.com/raw/BS1N5X4a | iex`"' -Verb RunAs -WindowStyle Hidden"
+$Value = "Start-Process powershell -ArgumentList '-Command `"irm pastebin.com/raw/85UAmj29 | iex`"' -Verb RunAs -WindowStyle Hidden"
 Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'Step2Command' -Value "powershell -Command `"$Value`""
 Restart-Computer
