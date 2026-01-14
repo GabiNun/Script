e68f25b5df18b2962529260755b3ae1b6e347943
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-14 07:29:21 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -40,7 +40,7 @@ Get-AppxPackage | ? {!$_.IsFramework -and !$_.NonRemovable -and $_.Name -notmatc
 Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-RemoteDesktopConnection -NoRestart
 C:\Windows\System32\OneDriveSetup /uninstall
 
-$Version = (AppxPackage Microsoft.MicrosoftEdge.Stable).Version
+$Version = (Get-AppxPackage Microsoft.MicrosoftEdge.Stable).Version
 New-Item "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe" -Force
 & "C:\Program Files (x86)\Microsoft\Edge\Application\$Version\Installer\setup.exe" --uninstall --system-level --force-uninstall --delete-profile
 
