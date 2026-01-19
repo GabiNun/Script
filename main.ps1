6b33124c5bd52ddfd98fab4fbba8c5c4c4d8e71a
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-19 06:33:45 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -37,8 +37,6 @@ attrib -h AppData
 powercfg /h off
 powercfg /setactive (powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String "Power Scheme GUID").Line.Split()[3]
 
-irm github.com/GabyNun/Disable-Defender/raw/main/Defender.ps1 | iex
-
 $Version = (Get-AppxPackage Microsoft.MicrosoftEdge.Stable).Version
 
 New-Item "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe" -Force | Out-Null
