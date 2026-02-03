8d5e6efed4f0a83ef9ec56558e5a3976b9bfd39d
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-04 00:23:47 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -66,10 +66,8 @@ Get-CimInstance Win32_ShadowCopy | Remove-CimInstance
 
 takeown /f C:\Windows\System32\AggregatorHost.exe | Out-Null
 takeown /f C:\Windows\System32\SmartScreen.exe | Out-Null
-takeown /f C:\Windows\System32\ctfmon.exe | Out-Null
 takeown /f C:\Windows\System32\LsaIso.exe | Out-Null
 
 icacls C:\Windows\System32\AggregatorHost.exe /deny Administrators:F | Out-Null
 icacls C:\Windows\System32\SmartScreen.exe /deny Administrators:F | Out-Null
-icacls C:\Windows\System32\ctfmon.exe /deny Administrators:F | Out-Null
 icacls C:\Windows\System32\LsaIso.exe /deny Administrators:F | Out-Null
