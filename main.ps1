81cba7ee1ddeeab7b85a188425032528482eaad8
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-03 21:17:18 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -33,7 +33,7 @@ attrib +h Documents
 attrib +h Contacts
 attrib -h AppData
 
-powercfg /h off
+powercfg /Hibernate Off
 powercfg /setactive (powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String "Power Scheme GUID").Line.Split()[3]
 
 $Appx = (Get-AppxPackage *SecHealthUI).PackageFullName
