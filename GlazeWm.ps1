1625bfdab8791e8d5b210616afe82b0e9683db06
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-04 15:31:41 +0200

    Update GlazeWm.ps1

--- GlazeWm.ps1
+++ GlazeWm.ps1
@@ -1,5 +1,3 @@
-$ProgressPreference = 'SilentlyContinue'
-
 irm github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windows\config.yaml
 irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
 irm github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\vcruntime140.dll
