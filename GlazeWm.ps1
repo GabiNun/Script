3f03d0cd944b55b43beeea4d463f75703be9f601
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-04 15:38:28 +0200

    Delete GlazeWm.ps1

--- GlazeWm.ps1
+++
@@ -1,6 +0,0 @@
-irm github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windows\config.yaml
-irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
-irm github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\vcruntime140.dll
-irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm-watcher.exe -Out C:\Windows\glazewm-watcher.exe
-
-glazewm
