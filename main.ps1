465a22d9eebd107588dc4dd67d2274097783bd60
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-05 12:53:01 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -52,6 +52,7 @@ foreach ($Package in (Get-ProvisionedAppPackage -Online).PackageName) {
 foreach ($feature in (Get-WindowsOptionalFeature -Online | Where-Object State -eq Enabled | Where-Object FeatureName -notmatch 'Defender').FeatureName) {
     Dism /Online /Disable-Feature /FeatureName:$feature /NoRestart | Out-Null
 }
+Dism /Online /Enable-Feature /FeatureName:Microsoft-Hyper-V-All /All /NoRestart | Out-Null
 
 $Packages =
     'Microsoft.WindowsCalculator',
@@ -94,8 +95,6 @@ Stop-Process -Name ApplicationFrameHost,Widgets
 foreach ($Package in $Packages) {
     Get-AppxPackage $Package | Remove-AppxPackage
 }
-
-Dism /Online /Enable-Feature /FeatureName:Microsoft-Hyper-V-All /All /NoRestart | Out-Null
 
 Unregister-ScheduledTask -Confirm:$False
 
