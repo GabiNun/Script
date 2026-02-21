d965ea29d903875a3df3280d4327fd49db1d11f3
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-21 12:03:00 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,7 +1,5 @@
 $ProgressPreference = 'SilentlyContinue'
 
-winget source remove msstore | Out-Null
-
 irm github.com/GabiNun/Script/raw/main/Registry.reg -Out Registry.reg
 irm github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windows\config.yaml
 irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
@@ -9,32 +7,34 @@ irm github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\
 irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm-watcher.exe -Out C:\Windows\glazewm-watcher.exe
 
 Start-Process regedit.exe -ArgumentList '/s Registry.reg'
-Start-Process OneDriveSetup.exe /uninstall
 Start-Process glazewm.exe
 
 Remove-Item Registry.reg
 
 $Files =
-    "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Windows Media Player Legacy.lnk",
-    "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\System Tools\Character Map.lnk",
-    "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Steps Recorder.lnk",
-    "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk",
+    "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Accessibility",
     "$Env:AppData\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk",
-    "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Accessibility","Links",
-    "$Env:Public","Saved Games","Videos",".glzr","Searches","Pictures","Music",
-    "Documents","Contacts","OneDrive","C:\Windows.old","C:\inetpub","Favorites"
+    "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk",
+    "$Env:Public",
+    "C:\PerfLogs",
+    "C:\Windows.old",
+    "Favorites",
+    "Links",
+    "Music",
+    "Pictures",
+    "Saved Games",
+    "Searches",
+    "Videos",
+    "Documents",
+    "Contacts"
 
 foreach ($File in $Files) {
     attrib +h $File
 }
+attrib -h AppData
 
 powercfg /Hibernate Off
 powercfg /Setactive (powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String "Power Scheme GUID").Line.Split()[3]
-
-$Appx = (Get-AppxPackage *SecHealthUI).PackageFullName
-$Sid = (Get-LocalUser $Env:UserName).Sid.Value
-
-New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force | Out-Null
 
 New-Item C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe -Force | Out-Null
 Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe" -ArgumentList '--uninstall --system-level --force-uninstall --delete-profile' -Wait
@@ -42,52 +42,6 @@ Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\set
 Stop-Process -Name SearchHost,*Edge* -Force
 Remove-Item "C:\Program Files (x86)\Microsoft" -Recurse -Force
 
-$Packages =
-    'Microsoft.WindowsCalculator',
-    'Microsoft.WindowsCamera',
-    'Microsoft.WindowsAlarms',
-    'Microsoft.WindowsFeedbackHub',
-    'Microsoft.ZuneMusic',
-    'Microsoft.MicrosoftOfficeHub',
-    'Microsoft.BingSearch',
-    'Clipchamp.Clipchamp',
-    'Microsoft.BingNews',
-    'MSTeams',
-    'Microsoft.WindowsNotepad',
-    'Microsoft.Todos',
-    'Microsoft.OutlookForWindows',
-    'Microsoft.Paint',
-    'Microsoft.Windows.Photos',
-    'Microsoft.PowerAutomateDesktop',
-    'MicrosoftCorporationII.QuickAssist',
-    'Microsoft.ScreenSketch',
-    'Microsoft.MicrosoftSolitaireCollection',
-    'Microsoft.WindowsSoundRecorder',
-    'Microsoft.MicrosoftStickyNotes',
-    'Microsoft.BingWeather',
-    'Microsoft.WebMediaExtensions',
-    'Microsoft.GamingApp',
-    'Microsoft.Xbox.TCUI',
-    'Microsoft.Windows.DevHome',
-    'Microsoft.GetHelp',
-    'Microsoft.WindowsStore',
-    'MicrosoftWindows.CrossDevice',
-    'Microsoft.ApplicationCompatibilityEnhancements',
-    'Microsoft.YourPhone',
-    'Microsoft.XboxGamingOverlay',
-    'MicrosoftWindows.Client.WebExperience',
-    'Microsoft.SecHealthUI'
-
-Stop-Process -Name Widgets
-foreach ($Package in $Packages) {
-    Get-AppxPackage $Package | Remove-AppxPackage
-}
-
-foreach ($Package in $Packages[0..($Packages.Count - 2)]) {
-    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $Package | Remove-AppxProvisionedPackage -Online | Out-Null
-}
-
-Dism /Online /Disable-Feature /FeatureName:Microsoft-RemoteDesktopConnection /NoRestart | Out-Null
 Dism /Online /Enable-Feature /FeatureName:Microsoft-Hyper-V-All /NoRestart | Out-Null
 
 Unregister-ScheduledTask -Confirm:$False
