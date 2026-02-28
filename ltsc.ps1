ced3c6af069f4ae494c914e8702c139e614fe02d
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-28 09:41:08 +0200

    Add files via upload

---
+++ ltsc.ps1
@@ -0,0 +1,64 @@
+$ProgressPreference = 'SilentlyContinue'
+
+irm github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -out DesktopAppInstaller.msixbundle
+irm github.com/microsoft/winget-cli/releases/latest/download/DesktopAppInstaller_Dependencies.zip -Out DesktopAppInstaller_Dependencies.zip
+
+Expand-Archive DesktopAppInstaller_Dependencies.zip
+Add-AppxPackage DesktopAppInstaller.msixbundle -DependencyPath DesktopAppInstaller_Dependencies\x64\*
+Remove-Item DesktopAppInstaller*
+
+irm github.com/GabiNun/Script/raw/main/Registry.reg -Out Registry.reg
+irm github.com/GabiNun/Script/raw/main/Defender.reg -Out Defender.reg
+irm github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windows\config.yaml
+irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
+irm github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\vcruntime140.dll
+irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm-watcher.exe -Out C:\Windows\glazewm-watcher.exe
+
+Register-ScheduledTask -TaskName Defender -Action (New-ScheduledTaskAction -Execute regedit.exe -Argument "/s $Home\Defender.reg") -User "NT SERVICE\TrustedInstaller" | Out-Null
+Start-ScheduledTask -TaskName Defender
+
+Start-Process regedit.exe -ArgumentList '/s Registry.reg'
+Start-Process glazewm.exe
+Start-Process mspaint.exe /uninstall
+Start-Process SnippingTool.exe /uninstall
+
+Remove-Item Registry.reg
+Remove-Item Defender.reg
+
+attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Accessibility"
+attrib +h "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Notepad.lnk"
+attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk"
+attrib +h "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Calculator.lnk"
+attrib +h "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk"
+attrib +h "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\System Tools\Character Map.lnk"
+attrib +h "$Env:Public"
+attrib +h "$Home\Favorites"
+attrib +h "$Home\Links"
+attrib +h "$Home\Music"
+attrib +h "$Home\Pictures"
+attrib +h "$Home\Saved Games"
+attrib +h "$Home\Searches"
+attrib +h "$Home\Videos"
+attrib +h "$Home\Documents"
+attrib +h "$Home\Contacts"
+attrib +h "$Home\.glzr"
+attrib +h "C:\Windows.old"
+attrib -h "$Home\AppData"
+
+powercfg /Hibernate Off
+powercfg /Setactive (powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String "Power Scheme GUID").Line.Split()[3]
+
+$Version = (Get-AppxPackage Microsoft.MicrosoftEdge.Stable).Version
+New-Item C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe -Force | Out-Null
+Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\$Version\Installer\setup.exe" -ArgumentList '--uninstall --system-level --force-uninstall --delete-profile' -Wait
+
+Stop-Process -Name SearchHost,*Edge* -Force
+Remove-Item "C:\Program Files (x86)\Microsoft" -Recurse -Force
+
+Dism /Online /Disable-Feature /FeatureName:Microsoft-RemoteDesktopConnection /NoRestart | Out-Null
+Dism /Online /Enable-Feature /FeatureName:Microsoft-Hyper-V-All /NoRestart | Out-Null
+
+Unregister-ScheduledTask -Confirm:$False
+Get-CimInstance Win32_PageFileSetting | Remove-CimInstance
+
+Stop-Process -Name Explorer
---
+++ main.ps1
@@ -0,0 +1,101 @@
+$ProgressPreference = 'SilentlyContinue'
+
+winget source remove msstore | Out-Null
+
+irm github.com/GabiNun/Script/raw/main/Registry.reg -Out Registry.reg
+irm github.com/GabiNun/Script/raw/main/Defender.reg -Out Defender.reg
+irm github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windows\config.yaml
+irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
+irm github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\vcruntime140.dll
+irm github.com/GabiNun/Script/raw/main/Glazewm/glazewm-watcher.exe -Out C:\Windows\glazewm-watcher.exe
+
+Start-Process regedit.exe -ArgumentList '/s Registry.reg'
+Start-Process OneDriveSetup.exe /uninstall
+Start-Process glazewm.exe
+
+Register-ScheduledTask -TaskName Defender -Action (New-ScheduledTaskAction -Execute regedit.exe -Argument "/s $Home\Defender.reg") -User "NT SERVICE\TrustedInstaller" | Out-Null
+Start-ScheduledTask -TaskName Defender
+
+Remove-Item Registry.reg
+Remove-Item Defender.reg
+
+$Files =
+    "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Windows Media Player Legacy.lnk",
+    "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\System Tools\Character Map.lnk",
+    "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Steps Recorder.lnk",
+    "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Administrative Tools.lnk",
+    "$Env:AppData\Microsoft\Windows\Start Menu\Programs\File Explorer.lnk",
+    "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Accessibility","Links",
+    "$Env:Public","Saved Games","Videos",".glzr","Searches","Pictures","Music",
+    "Documents","Contacts","OneDrive","C:\Windows.old","C:\inetpub","Favorites"
+
+foreach ($File in $Files) {
+    attrib +h $File
+}
+
+powercfg /Hibernate Off
+powercfg /Setactive (powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 | Select-String "Power Scheme GUID").Line.Split()[3]
+
+$Appx = (Get-AppxPackage *SecHealthUI).PackageFullName
+$Sid = (Get-LocalUser $Env:UserName).Sid.Value
+
+New-Item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$Sid\$Appx -Force | Out-Null
+
+New-Item C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe -Force | Out-Null
+Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe" -ArgumentList '--uninstall --system-level --force-uninstall --delete-profile' -Wait
+
+Stop-Process -Name SearchHost,*Edge* -Force
+Remove-Item "C:\Program Files (x86)\Microsoft" -Recurse -Force
+
+$Packages =
+    'Microsoft.WindowsCalculator',
+    'Microsoft.WindowsCamera',
+    'Microsoft.WindowsAlarms',
+    'Microsoft.WindowsFeedbackHub',
+    'Microsoft.ZuneMusic',
+    'Microsoft.MicrosoftOfficeHub',
+    'Microsoft.BingSearch',
+    'Clipchamp.Clipchamp',
+    'Microsoft.BingNews',
+    'MSTeams',
+    'Microsoft.WindowsNotepad',
+    'Microsoft.Todos',
+    'Microsoft.OutlookForWindows',
+    'Microsoft.Paint',
+    'Microsoft.Windows.Photos',
+    'Microsoft.PowerAutomateDesktop',
+    'MicrosoftCorporationII.QuickAssist',
+    'Microsoft.ScreenSketch',
+    'Microsoft.MicrosoftSolitaireCollection',
+    'Microsoft.WindowsSoundRecorder',
+    'Microsoft.MicrosoftStickyNotes',
+    'Microsoft.BingWeather',
+    'Microsoft.WebMediaExtensions',
+    'Microsoft.GamingApp',
+    'Microsoft.Xbox.TCUI',
+    'Microsoft.Windows.DevHome',
+    'Microsoft.GetHelp',
+    'Microsoft.WindowsStore',
+    'MicrosoftWindows.CrossDevice',
+    'Microsoft.ApplicationCompatibilityEnhancements',
+    'Microsoft.YourPhone',
+    'Microsoft.XboxGamingOverlay',
+    'MicrosoftWindows.Client.WebExperience',
+    'Microsoft.SecHealthUI'
+
+Stop-Process -Name Widgets -ErrorAction SilentlyContinue
+foreach ($Package in $Packages) {
+    Get-AppxPackage $Package | Remove-AppxPackage
+}
+
+foreach ($Package in $Packages[0..($Packages.Count - 2)]) {
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $Package | Remove-AppxProvisionedPackage -Online | Out-Null
+}
+
+Dism /Online /Disable-Feature /FeatureName:Microsoft-RemoteDesktopConnection /NoRestart | Out-Null
+Dism /Online /Enable-Feature /FeatureName:Microsoft-Hyper-V-All /NoRestart | Out-Null
+
+Unregister-ScheduledTask -Confirm:$False
+Get-CimInstance Win32_PageFileSetting | Remove-CimInstance
+
+Stop-Process -Name Explorer
