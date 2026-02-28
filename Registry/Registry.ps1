f9796d8f0136357990731929d99f9cc202da3491
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-28 10:22:38 +0200

    Update Registry.ps1

--- Registry/Registry.ps1
+++ Registry/Registry.ps1
@@ -1,4 +1,4 @@
-irm https://github.com/GabiNun/script/raw/main/Registry/Defender.reg -Out Registry.reg
+irm https://github.com/GabiNun/script/raw/main/Registry/Registry.reg -Out Registry.reg
 irm https://github.com/GabiNun/Script/raw/main/Registry/Defender.reg -Out Defender.reg
 
 Start-Process regedit.exe -ArgumentList '/s Registry.reg'
