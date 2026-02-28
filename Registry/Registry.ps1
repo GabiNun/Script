dcbf971ea01640142b84c05845492871522a07fc
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-28 09:54:37 +0200

    Update Registry.ps1

--- Registry/Registry.ps1
+++ Registry/Registry.ps1
@@ -2,3 +2,9 @@ irm https://github.com/GabiNun/script/raw/main/Registry/Defender.reg -Out Regist
 irm https://github.com/GabiNun/Script/raw/main/Registry/Defender.reg -Out Defender.reg
 
 Start-Process regedit.exe -ArgumentList '/s Registry.reg'
+
+Register-ScheduledTask -TaskName Defender -Action (New-ScheduledTaskAction -Execute regedit.exe -Argument "/s $Home\Defender.reg") -User "NT SERVICE\TrustedInstaller" | Out-Null
+Start-ScheduledTask -TaskName Defender
+
+Remove-Item Registry.reg
+Remove-Item Defender.reg
