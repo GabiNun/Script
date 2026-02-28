bc93c8c189217fb2cb77820049a77585cce498ce
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-28 09:50:53 +0200

    Create Glazewm.ps1

---
+++ Glazewm/Glazewm.ps1
@@ -0,0 +1,4 @@
+irm https://github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windows\config.yaml
+irm https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
+irm https://github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\vcruntime140.dll
+irm https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm-watcher.exe -Out C:\Windows\glazewm-watcher.exe
