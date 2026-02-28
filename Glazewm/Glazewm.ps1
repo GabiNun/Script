3c72362880722982f4bf83c183c085f9e63b76f0
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-28 09:51:28 +0200

    Update Glazewm.ps1

--- Glazewm/Glazewm.ps1
+++ Glazewm/Glazewm.ps1
@@ -2,3 +2,5 @@ irm https://github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windo
 irm https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
 irm https://github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\vcruntime140.dll
 irm https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm-watcher.exe -Out C:\Windows\glazewm-watcher.exe
+
+Start-Process glazewm.exe
