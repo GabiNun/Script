4ab2543eefc8bd57727668b63652d6c5718e72c3
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-03-06 12:10:25 +0200

    Update ViveTool.ps1

--- ViveTool.ps1
+++ ViveTool.ps1
@@ -1,6 +1,6 @@
 Invoke-WebRequest https://github.com/thebookisclosed/ViVe/releases/download/v0.3.4/ViVeTool-v0.3.4-IntelAmd.zip -OutFile ViVeTool.zip
 Expand-Archive ViVeTool.zip
 
-.\ViVeTool\ViVeTool.exe /disable /id:47205210
+.\ViVeTool\ViVeTool.exe /disable /id:47205210 | Out-Null
 
 Remove-Item ViVeTool* -Recurse
