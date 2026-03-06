1418575d69f784d03cbbc6d88f647deb5e7c2330
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-03-06 12:46:17 +0200

    Update Glazewm.ps1

--- Glazewm/Glazewm.ps1
+++ Glazewm/Glazewm.ps1
@@ -1,3 +1,6 @@
+Set-ItemProperty -Path HKCU:\Environment -Name GLAZEWM_CONFIG_PATH -Value C:\Windows\config.yaml
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name Glazewm -Value glazewm.exe
+
 irm https://github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windows\config.yaml
 irm https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
 irm https://github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\vcruntime140.dll
