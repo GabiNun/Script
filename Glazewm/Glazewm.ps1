5e9cc72282af803aec05e9e0d8c0cc584bbf778c
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-03-09 13:01:01 +0200

    Update Glazewm.ps1

--- Glazewm/Glazewm.ps1
+++ Glazewm/Glazewm.ps1
@@ -3,9 +3,9 @@ $ProgressPreference = 'SilentlyContinue'
 [Environment]::SetEnvironmentVariable("GLAZEWM_CONFIG_PATH","C:\Windows\config.yaml","Machine")
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name Glazewm -Value glazewm.exe
 
-irm https://github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -Out C:\Windows\config.yaml
-irm https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -Out C:\Windows\glazewm.exe
-irm https://github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -Out C:\Windows\vcruntime140.dll
-irm https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm-watcher.exe -Out C:\Windows\glazewm-watcher.exe
+Invoke-WebRequest https://github.com/GabiNun/Script/raw/main/Glazewm/config.yaml -OutFile C:\Windows\config.yaml
+Invoke-WebRequest https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm.exe -OutFile C:\Windows\glazewm.exe
+Invoke-WebRequest https://github.com/GabiNun/Script/raw/main/Glazewm/vcruntime140.dll -OutFile C:\Windows\vcruntime140.dll
+Invoke-WebRequest https://github.com/GabiNun/Script/raw/main/Glazewm/glazewm-watcher.exe -OutFile C:\Windows\glazewm-watcher.exe
 
 Start-Process glazewm.exe
